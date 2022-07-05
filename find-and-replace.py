f6c96b06cdb01f9d1b8d2e79909f6e9457dcb107
Emberlynn Bland <gdnghtgrl@gmail.com>
2022-07-05 10:50:32 -0600

    Update find-and-replace.py

--- find-and-replace.py
+++ find-and-replace.py
@@ -1,14 +1,10 @@
 import obsidian_link_converter
-
 filepath = ""
-
 choice = input("Which file are you converting from? obsidian or markdown: ")
-
 if choice == "obsidian":
     # todo: don't handle one link at a time, but rather pass a file directly and parse every instance of a link to replace
     filepath = input("Enter obsidian link to convert: ")
-    filepath = obsidian_link_converter.obsidian_to_markdown(filepath)
-    
+    filepath = obsidian_link_converter.obsidian_to_markdown(filepath) 
 elif choice == "markdown":
     obsidian_link_converter.markdown_to_obsidian(filepath)
     filepath = input("Enter markdown link to convert: ")
@@ -16,10 +12,5 @@ elif choice == "markdown":
 else:
     print("choice not understood, canceling...")
     input()
-
 print(filepath)
-
-# print(obsidian_link_converter.markdown_to_obsidian("![](images/eugh.jpg)"))
-# print(obsidian_link_converter.obsidian_to_markdown("![[whatever.TIFF]]"))
-
 input()
